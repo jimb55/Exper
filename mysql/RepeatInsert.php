e8020f3e4c1fadf362719403e136a1220a31539a
jimb55 <760785539@qq.com>
2018-05-24 10:44:39 +0800

    数据库test

--- mysql/RepeatInsert.php
+++ mysql/RepeatInsert.php
@@ -1,20 +1,32 @@
 <?php
-
 $mysql_conf = array(
-    'host'    => '172.16.47.134:3306',
-    'db'      => 'testb',
-    'db_user' => 'allperson',
-    'db_pwd'  => '123456',
+    'host'    => 'localhost:3306',
+    'db'      => 'test',
+    'db_user' => 'root',
+    'db_pwd'  => '_Jimb55!',
 );
-$pdo = new PDO("mysql:host=" . $mysql_conf['host'] . ";dbname=" . $mysql_conf['db'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
+
+try {
+    $pdo = new PDO("mysql:host=" . $mysql_conf['host'] . ";dbname=" . $mysql_conf['db'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
+}catch (Exception $e){
+    print_r($e);
+}
+
+
+$res = $pdo->query("select count(*),sum(time) from pierce left join gek_user as p on pierce.id = p.id  where pierce.name like '%".substr(uniqid(),1,1)."1%'");
+$res = $pdo->query("select count(*),sum(time) from pierce left join gek_user as p on pierce.id = p.id  where pierce.name like '%".substr(uniqid(),1,2)."2%'");
+$res = $pdo->query("select count(*),sum(time) from pierce left join gek_user as p on pierce.id = p.id  where pierce.name like '%".substr(uniqid(),1,3)."3%'");
+$res = $pdo->query("select sleep(1),123;");
+print_r($res);
+exit();
 $pdo->exec("set names 'utf8'");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 while (1){
     try{
         for($i=0;$i<10000;$i++){
-            $sql = "INSERT INTO `students` (sname) VALUES ('".substr(uniqid(),6)."')";
-            $pdo->exec($sql);
+            $sql = "INSERT INTO `pierce` (`name`,`key`,`time`) VALUES ('".uniqid()."','9','".time()."')";
+            $pdo->query($sql);
             echo $i.PHP_EOL;
         }
     }catch (Exception $e){
